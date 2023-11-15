c9314b680bc5f2a432deba7df05616fb63d8e16a
jal7823 <jal7823@gmail.com>
2023-11-15 10:46:53 -0300

    feat: :heavy_plus_sign: Add conditional to Details projects
    Add conditional to Details projects

--- src/pages/DetailsProjects.jsx
+++ src/pages/DetailsProjects.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import Title from "../components/Title";
 import { useEffect, useState } from "react";
 import Subtitle from "../components/Subtitle";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 function DetailsProjects() {
   const { id } = useParams();
@@ -22,6 +22,7 @@ function DetailsProjects() {
     fetchData();
   }, [id]);
 
+  console.log(Projects);
 
   return (
     <div className="bg-black text-white h-screen container mx-auto p-2">
@@ -30,7 +31,11 @@ function DetailsProjects() {
         <p className="mt-4">{Projects.descriptions}</p>
       </div>
 
-      <br /><br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
       <div className="flex justify-center">
         <Title content="Stack" />
@@ -55,91 +60,123 @@ function DetailsProjects() {
         {/* back-end */}
 
         <div className="flex flex-wrap justify-around p-4 bg-black ">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <div className="flex justify-center">
-              <Subtitle content="Back-End" />
-            </div>
-            <NavLink target="_blank" to={Projects.url}><img
-              className="w-full h-[300px] object-contain"
-              src={Projects.bImage}
-              alt="Sunset in the mountains"
-            /></NavLink>
-            <div className="px-6 py-4">
-              <div className="text-center font-bold text-xl mb-2">
-                Libraries
+          {Projects.backend && Projects.backend.length > 0 ? (
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="flex justify-center">
+                  <Subtitle content="Back-End" />
+                </div>
+                <NavLink target="_blank" to={Projects.url}>
+                  <img
+                    className="w-full h-[300px] object-contain"
+                    src={Projects.bImage}
+                    alt="Sunset in the mountains"
+                  />
+                </NavLink>
+                <div className="px-6 py-4">
+                  <div className="text-center font-bold text-xl mb-2">
+                    Libraries
+                  </div>
+                  <hr className="my-4" />
+                  <ul className="text-white text-base">
+                    {Projects.backend &&
+                      Projects.backend.map((el, index) => (
+                        <ul className="flex justify-between" key={index}>
+                          <li className="mr-2">{el.name}</li>{" "}
+                          <i className={el.icon}></i>
+                        </ul>
+                      ))}
+                  </ul>
+                </div>
+                <div className="text-center font-bold text-xl mb-2">Status</div>
+                <hr className="my-4 w-4/5 mx-auto" />
+
+                <div className="flex justify-between content-center items-center">
+                  <p className="p-4 ">{Projects.status}</p>
+                  <i className="fa fa-check"></i>
+                </div>
+
+                <div className="px-6 pt-4 pb-2 flex justify-between">
+                  <NavLink
+                    target="_blank"
+                    to={Projects.github}
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  >
+                    <i className="fa fa-github"></i>
+                  </NavLink>
+                  <NavLink
+                    target="_blank"
+                    to={Projects.url}
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  >
+                    <i className="fa fa-globe"></i>
+                  </NavLink>
+                </div>
               </div>
-              <hr className="my-4" />
-              <ul className="text-white text-base">
-                {Projects.backend &&
-                  Projects.backend.map((el, index) => (
-                    <ul className="flex justify-between" key={index}>
-                      <li className="mr-2">{el.name}</li>{" "}
-                      <i className={el.icon}></i>
-                    </ul>
-                  ))}
-              </ul>
             </div>
-            <div className="text-center font-bold text-xl mb-2">Status</div>
-            <hr className="my-4 w-4/5 mx-auto" />
-
-            <div className="flex justify-between content-center items-center">
-              <p className="p-4 ">{Projects.status}</p>
-              <i className="fa fa-check"></i>
-            </div>
-
-            <div className="px-6 pt-4 pb-2 flex justify-between">
-              <NavLink target="_blank" to={Projects.github} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                <i className="fa fa-github"></i>
-              </NavLink>
-              <NavLink target="_blank" to={Projects.url} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                <i className="fa fa-globe"></i>
-              </NavLink>
-            </div>
-          </div>
+          ) : (
+            <></>
+          )}
 
           {/* front-end */}
 
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <div className="flex justify-center">
-              <Subtitle content="Front-End" />
-            </div>
-            <NavLink target="_blank" to={Projects.url}><img
-              className="w-full h-[300px] object-contain"
-              src={Projects.fImage}
-              alt="Sunset in the mountains"
-            /></NavLink>
-            <div className="px-6 py-4">
-              <div className="text-center font-bold text-xl mb-2">
-                Libraries
+          {Projects.frontend && Projects.frontend.length > 0 ? (
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="flex justify-center">
+                  <Subtitle content="Front-End" />
+                </div>
+                <NavLink target="_blank" to={Projects.url}>
+                  <img
+                    className="w-full h-[300px] object-contain"
+                    src={Projects.fImage}
+                    alt="Sunset in the mountains"
+                  />
+                </NavLink>
+                <div className="px-6 py-4">
+                  <div className="text-center font-bold text-xl mb-2">
+                    Libraries
+                  </div>
+                  <hr className="my-4" />
+                  <ul className="text-white text-base">
+                    {Projects.frontend &&
+                      Projects.frontend.map((el, index) => (
+                        <ul className="flex justify-between" key={index}>
+                          <li className="mr-2">{el.name}</li>{" "}
+                          <i className={el.icon}></i>
+                        </ul>
+                      ))}
+                  </ul>
+                </div>
+                <div className="text-center font-bold text-xl mb-2">Status</div>
+                <hr className="my-4 w-4/5 mx-auto" />
+
+                <div className="flex justify-between content-center items-center">
+                  <p className="p-4 ">{Projects.status}</p>
+                  <i className="fa fa-check"></i>
+                </div>
+
+                <div className="px-6 pt-4 pb-2 flex justify-between">
+                  <NavLink
+                    target="_blank"
+                    to={Projects.github}
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  >
+                    <i className="fa fa-github"></i>
+                  </NavLink>
+                  <NavLink
+                    target="_blank"
+                    to={Projects.url}
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  >
+                    <i className="fa fa-globe"></i>
+                  </NavLink>
+                </div>
               </div>
-              <hr className="my-4" />
-              <ul className="text-white text-base">
-                {Projects.frontend &&
-                  Projects.frontend.map((el, index) => (
-                    <ul className="flex justify-between" key={index}>
-                      <li className="mr-2">{el.name}</li>{" "}
-                      <i className={el.icon}></i>
-                    </ul>
-                  ))}
-              </ul>
             </div>
-            <div className="text-center font-bold text-xl mb-2">Status</div>
-            <hr className="my-4 w-4/5 mx-auto" />
-
-            <div className="flex justify-between content-center items-center">
-              <p className="p-4 ">{Projects.status}</p>
-              <i className="fa fa-check"></i>
-            </div>
-
-            <div className="px-6 pt-4 pb-2 flex justify-between">
-              <NavLink target="_blank" to={Projects.github} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                <i className="fa fa-github"></i>
-              </NavLink>
-              <NavLink target="_blank" to={Projects.url} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                <i className="fa fa-globe"></i>
-              </NavLink>
-            </div>
-          </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
